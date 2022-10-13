0a6800c47c9152e967f22ed72db69b5df8950aed
Maru40 <umeda.4072@gmail.com>
2022-10-13 10:29:49 +0900

    I_Damageにコンストラクタ追加

--- VRGame03/Assets/Maruyama/Scripts/Interface/I_Damaged.cs
+++ VRGame03/Assets/Maruyama/Scripts/Interface/I_Damaged.cs
@@ -6,6 +6,11 @@ using UnityEngine;
 public struct DamageData
 {
     public float damageValue;   //ダメージ値
+
+    public DamageData(float value)
+    {
+        this.damageValue = value;
+    }
 }
 
 

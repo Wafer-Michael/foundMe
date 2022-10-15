af5068293cbcab0ef0d1e5fae03f3ece1284733a
Maru40 <umeda.4072@gmail.com>
2022-10-15 21:42:15 +0900

    UtilityDictionaryの生成

---
+++ VRGame03/Assets/Maruyama/Scripts/Utility/UtilityDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+namespace maru
+{
+    namespace UtilityDictionary
+    {
+
+        /// <summary>
+        /// DictionaryをInspectorで表示したいための拡張、
+        /// AwakeかStartでInsertInspectorData関数を呼んでください。
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        [Serializable]
+        public class Ex_Dictionary<TKey, TValue> : Dictionary<TKey, TValue>
+        {
+            [Serializable]
+            struct TypeDraw
+            {
+                public TKey key;
+                public TValue value;
+            }
+
+            [SerializeField]
+            List<TypeDraw> m_typeDraws = new List<TypeDraw>();
+
+            public Ex_Dictionary()
+                : base()
+            { }
+
+            /// <summary>
+            /// Inspectorに表示していた情報を挿入する処理
+            /// </summary>
+            public void InsertInspectorData()
+            {
+                foreach (var type in m_typeDraws)
+                {
+                    base.Add(type.key, type.value);
+                }
+                m_typeDraws.Clear();
+            }
+        }
+    }
+}
+
+

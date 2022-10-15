c72fd2c6fa0d206f60e7af1ed8a5a67d3c6bb272
Maru40 <umeda.4072@gmail.com>
2022-10-15 21:40:51 +0900

    HandInputerの制御を追加

--- VRGame03/Assets/Maruyama/Scripts/Hand/HandInputer.cs
+++ VRGame03/Assets/Maruyama/Scripts/Hand/HandInputer.cs
@@ -10,32 +10,140 @@ using UnityEngine;
 
 public class HandInputer : MonoBehaviour//, IMixedRealityHandJointHandler//, IMixedRealitySourceStateHandler
 {
-    /// <summary>
-    /// ジャンケンの状態種別
-    /// </summary>
-    [System.Serializable]
-    public enum HandRockPaperScissorsStatus
+
+    [SerializeField]
+    OVRCustomSkeleton m_debugSkeleton;
+
+    [SerializeField]
+    TMPro.TextMeshProUGUI m_debugText;    //DebugLender用のテクスチャ
+
+    const float DEFAULT_THRESHOLD = 0.8f;  //指の曲がり具合の闘値(1に近しいほど厳しい。)
+
+    private void Update()
     {
-        Paper,      //パー
-        Scissor,    //チョキ
-        Rock,       //グー
-        Nothing,
+        //デバッグ
+        if (IsIndexStraight(m_debugSkeleton))
+        {
+            m_debugText.text = "Straight";
+        }
+        else
+        {
+            m_debugText.text = "Magaru";
+        }
     }
 
     /// <summary>
-    /// 握り敷居値
-    /// 0.35 ：エディター上のクリック動作での限界敷居値
+    /// 指定した全てのBoneIDが直線状にあるかどうか調べる
     /// </summary>
-    [SerializeField, Range(-1, 1)]
-    private float m_grabThreshold = 0.35f;
+    /// <param name="threshold">閾値 1に近いほど厳しい</param>
+    /// <param name="boneids">確認したいボーンの配列</param>
+    /// <returns></returns>
+    static private bool IsStraight(OVRCustomSkeleton skeleton, float threshold, params OVRSkeleton.BoneId[] boneids)
+    {
+        if (boneids.Length < 3) { return false; }   //ボーンが少ないと調べようがないためfalse。
 
-    // Inspectorで内容を確認するため、
-    // DictionaryではなくListで保持する
+        Vector3? oldVec = null; //一度目のループは存在しないため。
+        var dot = 1.0f;
+
+        for (var index = 0; index < boneids.Length - 1; index++)
+        {
+            var v = (skeleton.CustomBones[(int)boneids[index + 1]].position - skeleton.CustomBones[(int)boneids[index]].position).normalized;
+            if (oldVec.HasValue)    //前のベクトルが存在するなら。
+            {
+                dot *= Vector3.Dot(v, oldVec.Value); //内積の値を総乗していく。
+            }
+            oldVec = v;//ひとつ前の指ベクトル。
+        }
+        return dot >= threshold; //指定したBoneIDの内積の総乗が閾値を超えていたら直線とみなす。
+    }
+
     /// <summary>
-    /// ジェスチャー状態
+    /// 親指が曲がっているかどうか
     /// </summary>
-    //[SerializeField, Tooltip("ジェスチャー状態")]
-    //private List<GestureState> p_GestureStateList;
+    /// <param name="skeleton">スケルトン</param>
+    /// <param name="threshold">閾値 1に近いほど厳しい</param>
+    /// <returns>真っすぐならtrue</returns>
+    static public bool IsThumbStraight(OVRCustomSkeleton skeleton, float threshold = DEFAULT_THRESHOLD)
+    {
+        OVRSkeleton.BoneId[] boneids = {
+            OVRSkeleton.BoneId.Hand_Thumb2,
+            OVRSkeleton.BoneId.Hand_Thumb3,
+            OVRSkeleton.BoneId.Hand_ThumbTip
+        };
 
+        return IsStraight(skeleton, threshold, boneids);
+    }
+
+    /// <summary>
+    /// 人差し指が曲がっているかどうか
+    /// </summary>
+    /// <param name="skeleton">スケルトン</param>
+    /// <param name="threshold">閾値 1に近いほど厳しい</param>
+    /// <returns>真っすぐならtrue</returns>
+    static public bool IsIndexStraight(OVRCustomSkeleton skeleton, float threshold = DEFAULT_THRESHOLD)
+    {
+        OVRSkeleton.BoneId[] boneids = {
+            OVRSkeleton.BoneId.Hand_Index1,
+            OVRSkeleton.BoneId.Hand_Index2,
+            OVRSkeleton.BoneId.Hand_Index3,
+            OVRSkeleton.BoneId.Hand_IndexTip
+        };
+
+        return IsStraight(skeleton, threshold, boneids);
+    }
+
+    /// <summary>
+    /// 中指が曲がっているかどうか
+    /// </summary>
+    /// <param name="skeleton">スケルトン</param>
+    /// <param name="threshold">閾値 1に近いほど厳しい</param>
+    /// <returns></returns>
+    static public bool IsMiddleStraight(OVRCustomSkeleton skeleton, float threshold = DEFAULT_THRESHOLD)
+    {
+        OVRSkeleton.BoneId[] boneids = {
+            OVRSkeleton.BoneId.Hand_Middle1,
+            OVRSkeleton.BoneId.Hand_Middle2,
+            OVRSkeleton.BoneId.Hand_Middle3,
+            OVRSkeleton.BoneId.Hand_MiddleTip
+        };
+
+        return IsStraight(skeleton, threshold, boneids);
+    }
+
+    /// <summary>
+    /// 薬指が曲がっているかどうか
+    /// </summary>
+    /// <param name="skeleton">スケルトン</param>
+    /// <param name="threshold">閾値 1に近いほど厳しい</param>
+    /// <returns></returns>
+    static public bool IsRingStraight(OVRCustomSkeleton skeleton, float threshold = DEFAULT_THRESHOLD)
+    {
+        OVRSkeleton.BoneId[] boneids = {
+            OVRSkeleton.BoneId.Hand_Ring1,
+            OVRSkeleton.BoneId.Hand_Ring2,
+            OVRSkeleton.BoneId.Hand_Ring3,
+            OVRSkeleton.BoneId.Hand_RingTip
+        };
+
+        return IsStraight(skeleton, threshold, boneids);
+    }
+
+    /// <summary>
+    /// 小指が曲がっているかどうか
+    /// </summary>
+    /// <param name="skeleton">スケルトン</param>
+    /// <param name="threshold">閾値 1に近いほど厳しい</param>
+    /// <returns></returns>
+    static public bool IsPinkyStraight(OVRCustomSkeleton skeleton, float threshold = DEFAULT_THRESHOLD)
+    {
+        OVRSkeleton.BoneId[] boneids = {
+            OVRSkeleton.BoneId.Hand_Pinky1,
+            OVRSkeleton.BoneId.Hand_Pinky2,
+            OVRSkeleton.BoneId.Hand_Pinky3,
+            OVRSkeleton.BoneId.Hand_PinkyTip
+        };
+
+        return IsStraight(skeleton, threshold, boneids);
+    }
 
 }

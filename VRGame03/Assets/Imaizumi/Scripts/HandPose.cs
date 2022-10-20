ab384c56dc9ccb9bbe2126e24407a8a7de9b997b
Shion Imaizumi <zeal20300052@c-league.jp>
2022-10-20 14:51:22 +0900

    ポーズを取得できるようにした

--- VRGame03/Assets/Imaizumi/Scripts/HandPose.cs
+++ VRGame03/Assets/Imaizumi/Scripts/HandPose.cs
@@ -4,25 +4,56 @@ using UnityEngine;
 
 public class HandPose : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum Pose
     {
-        
+        guu = 0x00,
+        paa = 0x1F,
+        kyoki = 0x06
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 現在のポーズを取得する
+    /// </summary>
+    public Pose GetPose(OVRCustomSkeleton skeleton)
     {
-        
+        System.Byte pose = 0x00; // 曲がっている指
+
+        if (HandInputer.IsThumbStraight(skeleton))
+        {
+            pose += 0x01;
+        }
+        if (HandInputer.IsIndexStraight(skeleton))
+        {
+            pose += 0x02;
+        }
+        if (HandInputer.IsMiddleStraight(skeleton))
+        {
+            pose += 0x04;
+        }
+        if (HandInputer.IsRingStraight(skeleton))
+        {
+            pose += 0x08;
+        }
+        if (HandInputer.IsPinkyStraight(skeleton))
+        {
+            pose += 0x10;
+        }
+
+        return (Pose)pose;
     }
 
-    bool IsOpen()
+    /// <summary>
+    /// そのポーズを取っているかを判定する
+    /// </summary>
+    /// <param name="pose">判定したいポーズ</param>
+    public bool IsPose(Pose pose, OVRCustomSkeleton skeleton)
     {
-        return false;
-    }
+        bool result = false;
 
-    bool IsGrip()
-    {
-        return false;
+        if (pose == GetPose(skeleton))
+        {
+            result = true;
+        }
+        return result;
     }
 }

9d22cd8d23791845a8cccb887f0024f088e0f959
Shion Imaizumi <zeal20300052@c-league.jp>
2022-10-20 14:50:08 +0900

    クラスを静的クラスにした

--- VRGame03/Assets/Imaizumi/Scripts/FingerID.cs
+++ VRGame03/Assets/Imaizumi/Scripts/FingerID.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FingerID : MonoBehaviour
+static class FingerID
 {
-    public readonly int[][] FingerId = new int[][]
+    static private readonly OVRSkeleton.BoneId[][] FingerId = new OVRSkeleton.BoneId[][]
     {
-        new int[] { (int)OVRSkeleton.BoneId.Hand_Thumb2, (int)OVRSkeleton.BoneId.Hand_Thumb3, (int)OVRSkeleton.BoneId.Hand_ThumbTip },
-        new int[] { (int)OVRSkeleton.BoneId.Hand_Index1, (int)OVRSkeleton.BoneId.Hand_Index2, (int)OVRSkeleton.BoneId.Hand_Index3, (int)OVRSkeleton.BoneId.Hand_IndexTip },
-        new int[] { (int)OVRSkeleton.BoneId.Hand_Middle1, (int)OVRSkeleton.BoneId.Hand_Middle2, (int)OVRSkeleton.BoneId.Hand_Middle3, (int)OVRSkeleton.BoneId.Hand_MiddleTip },
-        new int[] { (int)OVRSkeleton.BoneId.Hand_Ring1, (int)OVRSkeleton.BoneId.Hand_Ring2, (int)OVRSkeleton.BoneId.Hand_Ring3, (int)OVRSkeleton.BoneId.Hand_RingTip },
-        new int[] { (int)OVRSkeleton.BoneId.Hand_Pinky1, (int)OVRSkeleton.BoneId.Hand_Pinky2, (int)OVRSkeleton.BoneId.Hand_Pinky3, (int)OVRSkeleton.BoneId.Hand_PinkyTip }
+        new OVRSkeleton.BoneId[] { OVRSkeleton.BoneId.Hand_Thumb2,  OVRSkeleton.BoneId.Hand_Thumb3,  OVRSkeleton.BoneId.Hand_ThumbTip },                                   // Thumb
+        new OVRSkeleton.BoneId[] { OVRSkeleton.BoneId.Hand_Index1,  OVRSkeleton.BoneId.Hand_Index2,  OVRSkeleton.BoneId.Hand_Index3,  OVRSkeleton.BoneId.Hand_IndexTip },  // Index
+        new OVRSkeleton.BoneId[] { OVRSkeleton.BoneId.Hand_Middle1, OVRSkeleton.BoneId.Hand_Middle2, OVRSkeleton.BoneId.Hand_Middle3, OVRSkeleton.BoneId.Hand_MiddleTip }, // Middle
+        new OVRSkeleton.BoneId[] { OVRSkeleton.BoneId.Hand_Ring1,   OVRSkeleton.BoneId.Hand_Ring2,   OVRSkeleton.BoneId.Hand_Ring3,   OVRSkeleton.BoneId.Hand_RingTip },   // Ring
+        new OVRSkeleton.BoneId[] { OVRSkeleton.BoneId.Hand_Pinky1,  OVRSkeleton.BoneId.Hand_Pinky2,  OVRSkeleton.BoneId.Hand_Pinky3,  OVRSkeleton.BoneId.Hand_PinkyTip }   // Pinky
     };
 
     public enum Finger
@@ -22,9 +22,14 @@ public class FingerID : MonoBehaviour
         Pinky
     }
 
-    public int[] GetFinger(Finger id)
+    /// <summary>
+    /// 指のIDを取得する
+    /// </summary>
+    /// <param name="id">取得したい指</param>
+    /// <returns>指の骨の配列</returns>
+    static public OVRSkeleton.BoneId[] GetFinger(Finger id)
     {
-        int[] fingerID = FingerId[(int)id];
+        OVRSkeleton.BoneId[] fingerID = FingerId[(int)id];
 
         return fingerID;
     }

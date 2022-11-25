ce946cc17cc56804c4600561e6e0a577d483bc05
Shion Imaizumi <zeal20300052@c-league.jp>
2022-11-25 15:54:00 +0900

    プレイヤー移動

--- VRGame03/Assets/Imaizumi/Scripts/PC/PlayerPC.cs
+++ VRGame03/Assets/Imaizumi/Scripts/PC/PlayerPC.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerPC : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    float moveSpeed = 5.0f;
+
+    [SerializeField]
+    float rotationSpeed = 360.0f;
+
+    [SerializeField]
+    Camera camera;
+    Rigidbody rigid;
+
+    Animator animator;
     void Start()
     {
-        
+        rigid = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 forward = new Vector3(0.0f, 0.0f, 0.0f);
+
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (direction.sqrMagnitude > 0.01f)
+        {
+            forward = Vector3.Slerp(transform.forward, direction, rotationSpeed);
+        }
+
+        direction = maru.Utility.ConvartCameraVec(direction, camera, this.gameObject);
+        rigid.AddForce(direction * moveSpeed);
     }
 }

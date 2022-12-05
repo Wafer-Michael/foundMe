b697745b5c7ecf344e5113529be3b0d6ef1484e5
Maru40 <umeda.4072@gmail.com>
2022-12-05 14:38:08 +0900

    EnemyBaseに監視するターゲットの設定をできるようにした。

--- VRGame03/Assets/Maruyama/Scripts/Enemy/Component/EnemyBase.cs
+++ VRGame03/Assets/Maruyama/Scripts/Enemy/Component/EnemyBase.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class EnemyBase : MonoBehaviour
 {
-    
+    EyeSearchRange m_eyeRange;
+
+    [SerializeField]
+    private List<GameObject> m_observeIsInEyeTargetObjects = new List<GameObject>();
+
+    private ObserveIsInEyeTargets m_observeIsInEyeTargets;  //視界範囲に入ったらターゲット判定を取るターゲット
+    public ObserveIsInEyeTargets GetObserveIsInEyeTargets() => m_observeIsInEyeTargets;
+
+    private void Awake()
+    {
+        m_eyeRange = GetComponent<EyeSearchRange>();
+
+        m_observeIsInEyeTargets = new ObserveIsInEyeTargets(m_observeIsInEyeTargetObjects, m_eyeRange);
+    }
+
 }

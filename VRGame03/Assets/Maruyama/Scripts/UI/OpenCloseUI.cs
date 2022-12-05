4cd7de8fd541d9d76773c8af5830b3f5946d8976
Maru40 <umeda.4072@gmail.com>
2022-12-05 11:17:43 +0900

    OpneCloseUIの追加

---
+++ VRGame03/Assets/Maruyama/Scripts/UI/OpenCloseUI.cs
@@ -0,0 +1,37 @@
+using OculusSampleFramework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCloseUI : TouchBottunEventBase
+{
+    [SerializeField]
+    private List<GameObject> m_closeUIs;   //•Â‚¶‚éUI”z—ñ
+
+    [SerializeField]
+    private bool m_isDraw = false;
+    public bool IsDraw()
+    {
+        Debug.Log(m_isDraw);
+        return m_isDraw;
+    }
+
+    private void Start()
+    {
+        SetDraws(IsDraw());
+    }
+
+    public override void Touch(InteractableStateArgs obj)
+    {
+        SetDraws(!IsDraw());
+        m_isDraw = !m_isDraw;
+    }
+
+    private void SetDraws(bool isDraw)
+    {
+        foreach (var ui in m_closeUIs)
+        {
+            ui.SetActive(isDraw);
+        }
+    }
+}

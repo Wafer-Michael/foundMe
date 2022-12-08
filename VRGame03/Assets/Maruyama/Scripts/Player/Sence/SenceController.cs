7f1320658bac5c31a7682e1d6ff6bb8f5ea089f8
Maru40 <umeda.4072@gmail.com>
2022-12-08 09:42:53 +0900

    SceneControllerとInputerの追加

--- VRGame03/Assets/Maruyama/Scripts/Player/Sence/SenceController.cs
+++ VRGame03/Assets/Maruyama/Scripts/Player/Sence/SenceController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class SenceController : MonoBehaviour
 {
+    public enum State
+    { 
+        Normal,
+        Scene,
+    }
+
     static string[] DEFAULT_NORMAL_LAYERS = new string[] { "L_Obstacle" };
     static string[] DEFAULT_SENCE_LAYERS = new string[] { "L_Obstacle" };
 
@@ -16,13 +22,57 @@ public class SenceController : MonoBehaviour
     [SerializeField]
     private string[] m_senceEyeLayerStrings = DEFAULT_SENCE_LAYERS;
 
+    private State m_state = State.Normal;
+    public State CurrentState => m_state;
+
+    [SerializeField]
+    private float m_senceTime = 10.0f;
+
+    [SerializeField]
+    private float m_recastTime = 3.0f;
+
+    private GameTimer m_timer = new GameTimer();
+    private GameTimer m_recastTimer = new GameTimer();
+
     private void Update()
     {
-           
+        m_recastTimer.UpdateTimer();
+
+        if (PlayerInputer.IsSence() && m_recastTimer.IsTimeUp) {
+            StartSence();
+        }
+
+        if(CurrentState == State.Scene)
+        {
+            m_timer.UpdateTimer();
+            if (m_timer.IsTimeUp)
+            {
+                EndSence();
+            }
+        }
     }
 
     private void StartSence()
     {
-        //m_camera.cullingMask = ;
+        //ƒZƒ“ƒXó‘Ô‚È‚çˆ—‚ğ‚µ‚È‚¢B
+        if(CurrentState == State.Scene) {
+            return;
+        }
+
+        m_timer.ResetTimer(m_senceTime);
+        m_state = State.Scene;
+
+        var layer = LayerMask.GetMask(m_senceEyeLayerStrings);
+        m_camera.cullingMask = layer;
+    }
+
+    private void EndSence()
+    {
+        m_state = State.Normal;
+
+        var layer = LayerMask.GetMask(m_nomalEyeLayerStrings);
+        m_camera.cullingMask = layer;
+
+        m_recastTimer.ResetTimer(m_recastTime);
     }
 }

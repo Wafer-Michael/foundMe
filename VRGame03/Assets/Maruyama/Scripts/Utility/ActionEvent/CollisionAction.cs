5fa28af5bc974d4584a11f1343533bbee95daaeb
Maru40 <umeda.4072@gmail.com>
2022-12-14 12:36:54 +0900

    AcctionEvent系をインクルード

---
+++ VRGame03/Assets/Maruyama/Scripts/Utility/ActionEvent/CollisionAction.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CollisionAction : MonoBehaviour
+{
+    #region メンバ変数
+
+    [SerializeField]
+    protected UnityEvent<Collision> m_enterAction = null;
+    [SerializeField]
+    protected UnityEvent<Collision> m_stayAction = null;
+    [SerializeField]
+    protected UnityEvent<Collision> m_exitAction = null;
+
+    #endregion
+
+    #region Enter
+
+    public void AddEnterAction(UnityAction<Collision> action)
+    {
+        m_enterAction.AddListener(action);
+    }
+
+    public void SetEnterAction(UnityEvent<Collision> action)
+    {
+        m_enterAction = action;
+    }
+
+    #endregion
+
+    #region Stay
+
+    public void AddStayAction(UnityAction<Collision> action)
+    {
+        m_stayAction.AddListener(action);
+    }
+
+    public void SetStayAction(UnityEvent<Collision> action)
+    {
+        m_stayAction = action;
+    }
+
+    #endregion
+
+    #region Exit
+
+    public void AddExitAction(UnityAction<Collision> action)
+    {
+        m_exitAction.AddListener(action);
+    }
+
+    public void SetExitAction(UnityEvent<Collision> action)
+    {
+        m_exitAction = action;
+    }
+
+    #endregion
+
+    #region OnCollision
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        m_enterAction?.Invoke(collision);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        m_stayAction?.Invoke(collision);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        m_exitAction?.Invoke(collision);
+    }
+
+    #endregion
+}
---
+++ VRGame03/Assets/Maruyama/Scripts/Utility/ActionEvent/TriggerAction.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+using System;
+
+public class TriggerAction : MonoBehaviour
+{
+    #region メンバ変数
+
+    [SerializeField]
+    protected UnityEvent<Collider> m_enterAction = null;
+    [SerializeField]
+    protected UnityEvent<Collider> m_stayAction = null;
+    [SerializeField]
+    protected UnityEvent<Collider> m_exitAction = null;
+
+    #endregion
+
+    #region Enter
+
+    public void AddEnterAction(UnityAction<Collider> action)
+    {
+        m_enterAction.AddListener(action);
+    }
+
+    public void SetEnterAction(UnityEvent<Collider> action)
+    {
+        m_enterAction = action;
+    }
+
+    #endregion
+
+    #region Stay
+
+    public void AddStayAction(UnityAction<Collider> action)
+    {
+        m_stayAction.AddListener(action);
+    }
+
+    public void SetStayAction(UnityEvent<Collider> action)
+    {
+        m_stayAction = action;
+    }
+
+    #endregion
+
+    #region Exit
+
+    public void AddExitAction(UnityAction<Collider> action)
+    {
+        m_exitAction.AddListener(action);
+    }
+
+    public void SetExitAction(UnityEvent<Collider> action)
+    {
+        m_exitAction = action;
+    }
+
+    #endregion
+
+    #region OnTrigger
+
+    private void OnTriggerEnter(Collider other)
+    {
+        m_enterAction?.Invoke(other);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        m_stayAction?.Invoke(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        m_exitAction?.Invoke(other);
+    }
+
+    #endregion
+
+}

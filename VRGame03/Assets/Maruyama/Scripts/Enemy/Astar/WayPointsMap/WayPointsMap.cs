ef1323ce3083da4e07d3357e75a2155bfab73f05
Maru40 <umeda.4072@gmail.com>
2022-12-27 22:50:06 +0900

    WayPointsMapのCreateMap関数の実装

--- VRGame03/Assets/Maruyama/Scripts/Enemy/Astar/WayPointsMap/WayPointsMap.cs
+++ VRGame03/Assets/Maruyama/Scripts/Enemy/Astar/WayPointsMap/WayPointsMap.cs
@@ -2,7 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using GraphType = SparseGraph<AstarNode, AstarEdge>;
+
 public class WayPointsMap
 {
-    
+    private GraphType m_graph;  //グラフデータ
+
+    public WayPointsMap()
+    {
+        m_graph = new GraphType();
+    }
+
+    /// <summary>
+    /// ウェイポイントマップの生成
+    /// </summary>
+    public void CreateWayPointsMap(Factory.WayPointsMap_FloodFill.Parametor factoryParametor)
+    {
+        var factory = new Factory.WayPointsMap_FloodFill();
+
+        factory.AddWayPointMap(m_graph, factoryParametor);
+    }
+
+    //--------------------------------------------------------------------------------------
+    /// アクセッサ
+    //--------------------------------------------------------------------------------------
+
+    public void SetGraph(GraphType graph) { m_graph = graph; }
+
+    public GraphType GetGraph() { return m_graph; }
 }

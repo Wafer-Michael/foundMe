68c63361c2ebc9c1b8d4e7367a977ea46b0a1920
Maru40 <umeda.4072@gmail.com>
2022-12-28 15:28:35 +0900

    SparceGraphのノードの取得バグの修正

--- VRGame03/Assets/Maruyama/Scripts/Graph/SparseGraph.cs
+++ VRGame03/Assets/Maruyama/Scripts/Graph/SparseGraph.cs
@@ -27,11 +27,20 @@ public class SparseGraph<NodeType, EdgeType>
     }
 
     public NodeType GetNode(int index) {
-        if (index >= m_nodes.Count) {   //ノード数より大きいインデックスを指定したらnullを返す。
-            return null;
+        foreach(var node in m_nodes)
+        {
+            if(node.GetIndex() == index) {
+                return node;
+            }
         }
 
-        return m_nodes[index];
+        return null;
+
+        //if (index >= m_nodes.Count) {   //ノード数より大きいインデックスを指定したらnullを返す。
+        //    return null;
+        //}
+
+        //return m_nodes[index];
     }
 
     public List<NodeType> GetNodes() { 

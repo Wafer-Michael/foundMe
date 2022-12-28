5bd256739ae7a7a18b41ca52f3acf53633868239
Maru40 <umeda.4072@gmail.com>
2022-12-28 15:26:40 +0900

    DebugGraphDrawの実装

--- VRGame03/Assets/Maruyama/Scripts/Graph/DebugClass/DebugGraphDraw.cs
+++ VRGame03/Assets/Maruyama/Scripts/Graph/DebugClass/DebugGraphDraw.cs
@@ -2,17 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DebugGraphDraw : MonoBehaviour
+using GraphType = SparseGraph<AstarNode, AstarEdge>;
+
+public class DebugGraphDraw
 {
-    // Start is called before the first frame update
-    void Start()
+    private MonoBehaviour m_owner;            //このクラスの所有者
+    public MonoBehaviour Owner => m_owner;
+
+    readonly private GraphType m_graph;    //グラフ
+
+    private List<GameObject> m_nodes = new List<GameObject>();  //デバッグ用のノード
+    private List<GameObject> m_edges = new List<GameObject>();  //デバッグ用のエッジ
+
+    public DebugGraphDraw(MonoBehaviour owner, GraphType graph)
     {
-        
+        m_owner = owner;
+        m_graph = graph;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CreateDebugNodes(GameObject prefab)
     {
-        
+        foreach (var node in m_graph.GetNodes())
+        {
+            var drawObject = Object.Instantiate(prefab, node.GetPosition(), Quaternion.identity);
+            m_nodes.Add(drawObject);
+        }
     }
+
+    public void CreateDebugEdges(GameObject prefab)
+    {
+        foreach(var pair in m_graph.GetEdgesMap())
+        {
+            foreach(var edge in pair.Value)
+            {
+                var fromNode = m_graph.GetNode(edge.GetFromIndex());
+                var toNode = m_graph.GetNode(edge.GetToIndex());
+
+                var position = (fromNode.GetPosition() + toNode.GetPosition()) / 2.0f;
+
+                var drawObject = Object.Instantiate(prefab, position, Quaternion.identity);
+                m_edges.Add(drawObject);
+            }
+        }
+    }
+
+    //--------------------------------------------------------------------------------------
+    ///	アクセッサ
+    //--------------------------------------------------------------------------------------
+
 }

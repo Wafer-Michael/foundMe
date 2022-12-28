2be1aac2b0aa6fdaee6672900bcee8b05f74994f
Maru40 <umeda.4072@gmail.com>
2022-12-28 16:54:34 +0900

    Obstacleのバグ修正

--- VRGame03/Assets/Maruyama/Scripts/Utility/UtilityObstacle.cs
+++ VRGame03/Assets/Maruyama/Scripts/Utility/UtilityObstacle.cs
@@ -9,18 +9,18 @@ namespace maru
         [SerializeField]
         public static string[] DEFAULT_RAY_OBSTACLE_LAYER_STRINGS = new string[] { "L_Obstacle" };
 
-        public static bool IsRayObstacle(Vector3 startPosition, Vector3 targetPosition, float sphereRange = 0.5f)
+        public static bool IsRayObstacle(Vector3 startPosition, Vector3 targetPosition, float sphereRange = 0.1f)
         {
             return IsRayObstacle(startPosition, targetPosition, sphereRange, DEFAULT_RAY_OBSTACLE_LAYER_STRINGS);
         }
 
-        public static bool IsRayObstacle(Vector3 startPosition, Vector3 targetPosition, float sphereRange = 0.5f, params string[] obstacleLayers)
+        public static bool IsRayObstacle(Vector3 startPosition, Vector3 targetPosition, float sphereRange = 0.1f, params string[] obstacleLayers)
         {
             int obstacleLayer = LayerMask.GetMask(obstacleLayers);
 
             var colliders = Physics.OverlapSphere(startPosition, sphereRange, obstacleLayer);  //オブジェクトに接触時に透けるバグ解消用
 
-            return (!Physics.Linecast(startPosition, targetPosition, obstacleLayer) && colliders.Length == 0);
+            return (Physics.Linecast(startPosition, targetPosition, obstacleLayer) || colliders.Length != 0);
         }
     }
 }

3988a3804f07bdefdb8feae381b6e3635c3dc4a0
Maru40 <umeda.4072@gmail.com>
2022-12-30 10:39:15 +0900

    Cellの生成

---
+++ VRGame03/Assets/Maruyama/Scripts/CellMap/Cell.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : I_GraphNode
+{
+	//--------------------------------------------------------------------------------------
+	///	セルパラメータ
+	//--------------------------------------------------------------------------------------
+	public struct Parametor
+	{
+		public maru.Rect rect;
+
+		public Parametor(maru.Rect rect)
+        {
+			this.rect = rect;
+        }
+	}
+
+	private bool m_isActive;
+
+	private int m_index;
+
+	private Parametor m_param;
+
+	public Cell(int index, Parametor param)
+    {
+		m_isActive = true;
+		m_index = index;
+		m_param = param;
+	}
+
+	//--------------------------------------------------------------------------------------
+	///	アクセッサ
+	//--------------------------------------------------------------------------------------
+
+	public void SetPosition(Vector3 position) { m_param.rect.centerPosition = position; }
+
+	public Vector3 GetPosition() { return m_param.rect.centerPosition; }
+
+	public void SetWidth(float width) { m_param.rect.width = width; }
+
+	public float GetWidth() { return m_param.rect.width; }
+
+	public void SetDepth(float depth) { m_param.rect.depth = depth; }
+
+	public float GetDepth() { return m_param.rect.depth; }
+
+	public void SetParametor(Parametor parametor) { m_param = parametor; }
+
+	public Parametor GetParametor() { return m_param; }
+
+	public void SetIndex(int index) { m_index = index; }
+
+	public int GetIndex() { return m_index; }
+
+	public bool IsActive() { return m_isActive; }
+
+}

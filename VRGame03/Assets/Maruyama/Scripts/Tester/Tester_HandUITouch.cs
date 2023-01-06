bd93499a827ef58e5e50e6c74a3b0614b1b0283f
Maru40 <umeda.4072@gmail.com>
2023-01-07 03:42:30 +0900

    TestTouchUIの実装

---
+++ VRGame03/Assets/Maruyama/Scripts/Tester/Tester_HandUITouch.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester_HandUITouch : MonoBehaviour
+{
+    private TMPro.TextMeshProUGUI m_debugText;
+}

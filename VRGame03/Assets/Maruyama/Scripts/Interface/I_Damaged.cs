76eafff448c1e35c3e210aa3dcc32b16b6f2e7ab
Maru40 <umeda.4072@gmail.com>
2023-01-06 13:22:08 +0900

    I_DamagedにCollisionデータを追加できるようにした。

--- VRGame03/Assets/Maruyama/Scripts/Interface/I_Damaged.cs
+++ VRGame03/Assets/Maruyama/Scripts/Interface/I_Damaged.cs
@@ -7,11 +7,17 @@ public struct DamageData
 {
     public float damageValue;       //ダメージ値
     public GameObject attacker;     //ダメージを与えて来た相手
+    public Collider otherCollider;  //当たり判定に当たったコライダー
 
-    public DamageData(float value, GameObject attacker)
+    public DamageData(float value, GameObject attacker):
+        this(value, attacker, null)
+    { }
+
+    public DamageData(float value, GameObject attacker, Collider otherCollider)
     {
         this.damageValue = value;
         this.attacker = attacker;
+        this.otherCollider = otherCollider;
     }
 }
 

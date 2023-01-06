35a4c3d3a1c3be4b792f962d3e73d5dc5f533b65
Maru40 <umeda.4072@gmail.com>
2023-01-06 13:20:48 +0900

    BreakeStatusのI＿Damageの実装を消去

--- VRGame03/Assets/Maruyama/Scripts/Mesh/BreakableStatus.cs
+++ VRGame03/Assets/Maruyama/Scripts/Mesh/BreakableStatus.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BreakableStatus : MonoBehaviour, I_Damaged
+using System.Linq;
+
+public class BreakableStatus : MonoBehaviour
 {
     public static readonly Parametor DEFAULT_PARAMETOR = new Parametor(3.0f);
 
@@ -21,43 +23,65 @@ public class BreakableStatus : MonoBehaviour, I_Damaged
     private Parametor m_param = DEFAULT_PARAMETOR;  //パラメータ
     public Parametor Param => m_param;
 
-    private GK.BreakableSurface m_breakableSurface; //壊れる処理をまとめた者(子クラスとして作る)
+    //private GK.BreakableSurface m_breakableSurface; //壊れる処理をまとめた者(子クラスとして作る)
 
     private void Awake()
     {
-        m_breakableSurface = GetComponentInChildren<GK.BreakableSurface>();
+        //m_breakableSurface = GetComponentInChildren<GK.BreakableSurface>();
     }
 
-    public void Damaged(DamageData data)
+    public void Damaged(DamageData data, GK.BreakableSurface breakableSurface)
     {
         m_param.hp += -data.damageValue;
 
-        if (IsDead()) {
+        if (IsDead()) { //死亡したなら
             m_param.hp = 0.0f;
             DeadProcess();
         }
 
         //メッシュを割る処理
-        var hitPoint = CalculationHitPoint(data);   //当たった方向を計算
-        Break(hitPoint);
+        breakableSurface?.Break(breakableSurface.transform.InverseTransformPoint(CalculationHitPoint(data)));
+        //Break(data);
     }
 
-    private Vector2 CalculationHitPoint(DamageData data)
+    private Vector3 CalculationHitPoint(DamageData data)
     {
-        var result = Vector2.zero;
-
         //ここでヒットポイントを計算
-
-        return result;
+        var hitPoint = data.otherCollider.ClosestPoint(data.attacker.transform.position);
+        return hitPoint;
     }
 
     /// <summary>
-    /// メッシュを割る処理
+    /// メッシュを割る処理(現在使用していない)
     /// </summary>
-    /// <param name="position">当たった場所</param>
-    private void Break(Vector2 position)
+    /// <param name="data">ダメージデータ</param>
+    private void Break(DamageData data)
     {
-        m_breakableSurface.Break(position);
+        if (!data.otherCollider) {
+            Debug.Log("BreakableStatus::Break(DamageData data) : Colliderが存在しないため、メッシュを割れない");
+            return;
+        }
+
+        var breakableSurface = data.otherCollider.GetComponent<GK.BreakableSurface>();
+        if(breakableSurface == null) {
+            Debug.Log("BreakableStatus::Break(DamageData data) : BreakableSurfaceが存在しないため、メッシュを割れない");
+            return;
+        }
+
+        var hitPoint = CalculationHitPoint(data);   //当たった場所の計算
+        var children = GetComponentsInChildren<GK.BreakableSurface>();
+
+        //一番近い場所を取得するためにソート
+        var sortList = children.OrderBy(value =>
+            {
+                var childHitPoint = value.Collider.ClosestPoint(hitPoint);
+                return (childHitPoint - hitPoint).magnitude;
+            }
+        );
+
+        //一番近いターゲットでブレイクさせる。
+        var target = sortList.ToArray()[0];
+        target.Break(target.transform.InverseTransformPoint(hitPoint));
     }
 
     private void DeadProcess()

d50c06b71be90e517ba2adc6c483bbe94443b526
Maru40 <umeda.4072@gmail.com>
2023-01-07 16:54:32 +0900

    UIScrollControllerの実装

--- VRGame03/Assets/Maruyama/Scripts/UI/UIController/UIScrollController.cs
+++ VRGame03/Assets/Maruyama/Scripts/UI/UIController/UIScrollController.cs
@@ -12,12 +12,14 @@ public class UIScrollController : MonoBehaviour
     [SerializeField]
     private Vector2 m_minScope = new Vector2(-0.1f, -0.1f); //動ける最小距離
 
+    private Vector3 m_initializePosition;
     private GameObject m_initializeObject;
 
     private bool m_isTouch = false;
 
     private void Awake()
     {
+        m_initializePosition = transform.position;
         m_initializeObject = new GameObject("TouchUIInitialize");
         m_initializeObject.transform.position = transform.position;
         m_initializeObject.transform.rotation = transform.rotation;
@@ -61,13 +63,13 @@ public class UIScrollController : MonoBehaviour
     /// <returns>計算した位置</returns>
     public Vector3 CalculatePosition(PointerEvent pointerEvent)
     {
-        var toPoint = m_initializeObject.transform.InverseTransformPoint(pointerEvent.Pose.position);
+        var toPoint = maru.Utility.InverseTransformPoint(m_initializePosition, transform.rotation, pointerEvent.Pose.position);
 
         //それぞれのクランプ
         toPoint.x = Mathf.Clamp(toPoint.x, m_minScope.x, m_maxScope.x);
         toPoint.y = Mathf.Clamp(toPoint.y, m_minScope.y, m_maxScope.y);
 
-        return m_initializeObject.transform.position + (transform.rotation * toPoint);
+        return m_initializePosition + (transform.rotation * toPoint);
     }
 
 }

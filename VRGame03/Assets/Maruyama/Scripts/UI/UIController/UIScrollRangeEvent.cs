8d39c59260f684f43d25761acd6c6e3e885e1845
Maru40 <umeda.4072@gmail.com>
2023-01-10 12:34:10 +0900

    UIScrollRangeEventの実装

--- VRGame03/Assets/Maruyama/Scripts/UI/UIController/UIScrollRangeEvent.cs
+++ VRGame03/Assets/Maruyama/Scripts/UI/UIController/UIScrollRangeEvent.cs
@@ -57,4 +57,11 @@ public class UIScrollRangeEvent : MonoBehaviour
 
         return currentRange >= m_range;
     }
+
+    //--------------------------------------------------------------------------------------
+    /// アクセッサ
+    //--------------------------------------------------------------------------------------
+
+    private float GetRange() { return m_range; }
+
 }

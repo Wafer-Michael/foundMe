bcfc6f0af307eaa5ff4e56659ad7b40408c4e44e
Maru40 <umeda.4072@gmail.com>
2023-01-12 00:39:26 +0900

    JackCameraUIの変更

--- VRGame03/Assets/Maruyama/Scripts/UI/Jack/JackCameraUI.cs
+++ VRGame03/Assets/Maruyama/Scripts/UI/Jack/JackCameraUI.cs
@@ -7,28 +7,30 @@ public class JackCameraUI : MonoBehaviour
     [SerializeField]
     private RenderTexture m_renderTexture;
 
-    private Jackable m_currentJakable = null;
-
-    private void Awake()
-    {
-        
-    }
+    private Jackable m_currentJackable = null;
 
     /// <summary>
     /// 選択時のイベント
     /// </summary>
-    public void SelectEvent(Jackable jakable) 
+    public void SelectEvent(Jackable jackable) 
     {
-        jakable.GetEyeCamera().targetTexture = m_renderTexture;
-        m_currentJakable = jakable;
+        jackable.GetEyeCamera().targetTexture = m_renderTexture;
+        m_currentJackable = jackable;
+
+        Debug.Log("★: Select");
     }
 
     /// <summary>
     /// 選択解除時のイベント
     /// </summary>
-    public void UnSelectEvent()
+    public void UnSelectEvent(Jackable jackable)
     {
-        m_currentJakable.GetEyeCamera().targetTexture = null;
-        m_currentJakable = null;
+        m_currentJackable.GetEyeCamera().targetTexture = null;
+
+        if (m_currentJackable == jackable) {
+            m_currentJackable = null;
+        }
+
+        Debug.Log("★: UnSelect");
     }
 }

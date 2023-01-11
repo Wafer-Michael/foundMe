3179e1f725cc2e60af30a8cc75c55434fa3c2700
Maru40 <umeda.4072@gmail.com>
2023-01-12 00:46:07 +0900

    SelectableのInvoke部分を親クラスに委任

--- VRGame03/Assets/Maruyama/Scripts/UI/Selectable_VRUI.cs
+++ VRGame03/Assets/Maruyama/Scripts/UI/Selectable_VRUI.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 using UniRx;
 using Oculus.Interaction;
 
+using UnityEngine.Events;
+
 /// <summary>
 /// タッチしたときに選択状態が切り替わるUI
 /// </summary>
@@ -12,6 +14,13 @@ public class Selectable_VRUI : VRUI_Ex
 {
     private UniRx.ReactiveProperty<bool> m_isSelect = new ReactiveProperty<bool>(false);
     public System.IObservable<bool> ObservableIsSelect => m_isSelect;
+
+    [SerializeField]
+    private UnityEvent<Selectable_VRUI> m_selectEvents;
+
+    public void AddSelectEvent(UnityAction<Selectable_VRUI> action) { m_selectEvents.AddListener(action); }
+
+    public void SelectEventsInvoke() { m_selectEvents.Invoke(this); }
 
     public void Touch_Select() { m_isSelect.Value = !m_isSelect.Value; }
 

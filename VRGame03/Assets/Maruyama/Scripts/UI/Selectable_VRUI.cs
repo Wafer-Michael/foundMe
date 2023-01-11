c4b20068e0ada0621fdbef6e5188f182a7698124
Maru40 <umeda.4072@gmail.com>
2023-01-11 15:15:51 +0900

    Selectable_VRUIの生成

---
+++ VRGame03/Assets/Maruyama/Scripts/UI/Selectable_VRUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// タッチしたときに選択状態が切り替わるUI
+/// </summary>
+public class Selectable_VRUI : MonoBehaviour
+{
+    
+}

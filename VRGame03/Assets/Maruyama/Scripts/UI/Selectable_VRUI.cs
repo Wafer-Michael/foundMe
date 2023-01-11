2b34e93baa015f8370d614916f61fe9ee9f51bed
Maru40 <umeda.4072@gmail.com>
2023-01-12 00:39:51 +0900

    Selectable_VRUIで選択時に呼びたいイベントを追加

--- VRGame03/Assets/Maruyama/Scripts/UI/Selectable_VRUI.cs
+++ VRGame03/Assets/Maruyama/Scripts/UI/Selectable_VRUI.cs
@@ -2,10 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UniRx;
+using Oculus.Interaction;
+
 /// <summary>
 /// タッチしたときに選択状態が切り替わるUI
 /// </summary>
-public class Selectable_VRUI : MonoBehaviour
+public class Selectable_VRUI : VRUI_Ex
 {
-    
+    private UniRx.ReactiveProperty<bool> m_isSelect = new ReactiveProperty<bool>(false);
+    public System.IObservable<bool> ObservableIsSelect => m_isSelect;
+
+    public void Touch_Select() { m_isSelect.Value = !m_isSelect.Value; }
+
+    public void SetIsSelect(bool isSelect) { m_isSelect.Value = isSelect; }
+
+    public bool IsSelect() { return m_isSelect.Value; }
 }

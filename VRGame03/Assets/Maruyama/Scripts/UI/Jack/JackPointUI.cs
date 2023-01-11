10f8cabed049ff22a59675b5a964d4fa58bf6a67
Maru40 <umeda.4072@gmail.com>
2023-01-12 00:39:10 +0900

    JackPointUIでSelectEventsの実装

--- VRGame03/Assets/Maruyama/Scripts/UI/Jack/JackPointUI.cs
+++ VRGame03/Assets/Maruyama/Scripts/UI/Jack/JackPointUI.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JackPointUI : MonoBehaviour
+using UnityEngine.Events;
+
+public class JackPointUI : Selectable_VRUI
 {
-    private Jackable m_jakable = null;  //自分の設定されているハッキングされる物
+    private Jackable m_jakable = null;      //自分の設定されているハッキングされる物
+
+    [SerializeField]
+    private UnityEvent<JackPointUI> m_selectEvents;
+
+    public void AddSelectEvent(UnityAction<JackPointUI> action) { m_selectEvents.AddListener(action); }
+
+    public void SelectEventsInvoke() { m_selectEvents.Invoke(this); }
 
     //--------------------------------------------------------------------------------------
     /// アクセッサ

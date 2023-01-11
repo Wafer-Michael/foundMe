92274a9842c7a5cdc5c387ec8ea68a5197b3251f
Maru40 <umeda.4072@gmail.com>
2023-01-12 01:14:23 +0900

    JackPointUIの生成

--- VRGame03/Assets/Maruyama/Scripts/UI/Jack/JackPointUI.cs
+++ VRGame03/Assets/Maruyama/Scripts/UI/Jack/JackPointUI.cs
@@ -6,21 +6,5 @@ using UnityEngine.Events;
 
 public class JackPointUI : Selectable_VRUI
 {
-    private Jackable m_jakable = null;      //自分の設定されているハッキングされる物
-
-    [SerializeField]
-    private UnityEvent<JackPointUI> m_selectEvents;
-
-    public void AddSelectEvent(UnityAction<JackPointUI> action) { m_selectEvents.AddListener(action); }
-
-    public void SelectEventsInvoke() { m_selectEvents.Invoke(this); }
-
-    //--------------------------------------------------------------------------------------
-    /// アクセッサ
-    //--------------------------------------------------------------------------------------
-
-    public void SetJakable(Jackable jakable) { m_jakable = jakable; }
-
-    public Jackable GetJakable() { return m_jakable; }
 
 }

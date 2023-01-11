65f8a071e72944f7530985b63511781390e29769
Maru40 <umeda.4072@gmail.com>
2023-01-12 01:36:04 +0900

    JackCameraUIのいらない処理削除

--- VRGame03/Assets/Maruyama/Scripts/UI/Jack/JackCameraUI.cs
+++ VRGame03/Assets/Maruyama/Scripts/UI/Jack/JackCameraUI.cs
@@ -5,32 +5,10 @@ using UnityEngine;
 public class JackCameraUI : MonoBehaviour
 {
     [SerializeField]
-    private RenderTexture m_renderTexture;
+    private FadeObject m_fadeComponent;
 
-    private Jackable m_currentJackable = null;
-
-    /// <summary>
-    /// 選択時のイベント
-    /// </summary>
-    public void SelectEvent(Jackable jackable) 
+    public void FadeStart(FadeObject.FadeType fadeType)
     {
-        jackable.GetEyeCamera().targetTexture = m_renderTexture;
-        m_currentJackable = jackable;
-
-        Debug.Log("★: Select");
-    }
-
-    /// <summary>
-    /// 選択解除時のイベント
-    /// </summary>
-    public void UnSelectEvent(Jackable jackable)
-    {
-        m_currentJackable.GetEyeCamera().targetTexture = null;
-
-        if (m_currentJackable == jackable) {
-            m_currentJackable = null;
-        }
-
-        Debug.Log("★: UnSelect");
+        m_fadeComponent.FadeStart(fadeType);
     }
 }

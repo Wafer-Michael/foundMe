5375681601faf43b03a4de074f72468206cb50b6
Maru40 <umeda.4072@gmail.com>
2023-01-16 15:55:03 +0900

    Itemにステート情報を追加

--- VRGame03/Assets/Maruyama/Scripts/Item/Item.cs
+++ VRGame03/Assets/Maruyama/Scripts/Item/Item.cs
@@ -2,7 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UniRx;
+
 public class Item : MonoBehaviour
 {
-    
+    public enum State
+    {
+        Idle,           //初期状態
+        Access,         //アクセス状態
+        Getable,        //取得された状態
+    } 
+
+    private ReactiveProperty<State> m_state = new ReactiveProperty<State>(State.Idle);
+    public State CurrentState => m_state.Value;
+    public void SetState(State state) { m_state.Value = state; }
+    public System.IObservable<State> StateObservable => m_state;
 }

3377a8952127cf44b8dc74ee1227bb9b03b6f4da
Maru40 <umeda.4072@gmail.com>
2023-01-17 13:37:28 +0900

    CellMapで横の長さと縦の長さを設定できるようにした。

--- VRGame03/Assets/Maruyama/Scripts/CellMap/CellMap.cs
+++ VRGame03/Assets/Maruyama/Scripts/CellMap/CellMap.cs
@@ -2,13 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// セルフィールドデータ
+/// </summary>
+public struct CellMapFieldData
+{
+    public float width;     //横の長さ
+    public float depth;     //奥行きの長さ
+
+    public CellMapFieldData(float width, float depth)
+    {
+        this.width = width;
+        this.depth = depth;
+    }
+}
+
 public class CellMap<CellType>
     where CellType : Cell
 {
-    private List<CellType> m_cells;	//セルマップのセル配列
+    private List<CellType> m_cells = new List<CellType>();	        //セルマップのセル配列
+
+    private CellMapFieldData m_fieldData = new CellMapFieldData();  //セルマップのフィールドデータ
 
     public CellMap() {
         m_cells = new List<CellType>();
+        m_fieldData = new CellMapFieldData();
     }
 
     //--------------------------------------------------------------------------------------
@@ -18,6 +36,10 @@ public class CellMap<CellType>
     public void SetCells(List<CellType> cells) { m_cells = cells; }
     
     public List<CellType> GetCells() { return m_cells; }
+
+    public void SetFieldData(CellMapFieldData data) { m_fieldData = data; }
+
+    public CellMapFieldData GetFieldData() { return m_fieldData; }
 
 
     //--------------------------------------------------------------------------------------

c98881801db82c57a733dcca423fa0381a3d1b11
Maru40 <umeda.4072@gmail.com>
2023-01-17 14:33:17 +0900

    CellMapで八方向のセルを取得できるようにした。

--- VRGame03/Assets/Maruyama/Scripts/CellMap/CellMap.cs
+++ VRGame03/Assets/Maruyama/Scripts/CellMap/CellMap.cs
@@ -7,13 +7,13 @@ using UnityEngine;
 /// </summary>
 public struct CellMapFieldData
 {
-    public float width;     //横の長さ
-    public float depth;     //奥行きの長さ
+    public int widthCount;     //横の長さ
+    public int depthCount;     //奥行きの長さ
 
-    public CellMapFieldData(float width, float depth)
+    public CellMapFieldData(int widthCount, int depthCount)
     {
-        this.width = width;
-        this.depth = depth;
+        this.widthCount = widthCount;
+        this.depthCount = depthCount;
     }
 }
 
@@ -29,6 +29,40 @@ public class CellMap<CellType>
         m_fieldData = new CellMapFieldData();
     }
 
+    /// <summary>
+    /// 指定したインデックスの八方向のセルを取得する。
+    /// </summary>
+    /// <param name="currentIndex">開始セルの位置</param>
+    /// <returns></returns>
+    public List<CellType> SerchEightDirectionCells(int currentIndex)
+    {
+        var result = new List<CellType>();
+
+        //設定したいインデックス配列
+        int[] indices = {
+            currentIndex - 1,
+            currentIndex + 1,
+            currentIndex + (m_fieldData.widthCount),
+            currentIndex + (m_fieldData.widthCount + 1),
+            currentIndex + (m_fieldData.widthCount - 1),
+            currentIndex + (m_fieldData.depthCount),
+            currentIndex + (m_fieldData.depthCount + 1),
+            currentIndex + (m_fieldData.depthCount - 1)
+        };
+
+        //追加する。
+        foreach(int index in indices)
+        {
+            //0以上、かつ、Count以下なら、インデックスオーバーしない
+            if(0 <= index && index < m_cells.Count)
+            {
+                result.Add(m_cells[index]);
+            }
+        }
+
+        return result;
+    }
+
     //--------------------------------------------------------------------------------------
     ///	アクセッサ
     //--------------------------------------------------------------------------------------
@@ -40,7 +74,6 @@ public class CellMap<CellType>
     public void SetFieldData(CellMapFieldData data) { m_fieldData = data; }
 
     public CellMapFieldData GetFieldData() { return m_fieldData; }
-
 
     //--------------------------------------------------------------------------------------
     /// デバッグ

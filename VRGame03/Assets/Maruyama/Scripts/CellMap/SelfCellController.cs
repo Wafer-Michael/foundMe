fd57fed5ab8654ab7ad2196cc0d5258b0cc49067
Maru40 <umeda.4072@gmail.com>
2023-01-17 13:33:14 +0900

    SelfCellControllerの生成

---
+++ VRGame03/Assets/Maruyama/Scripts/CellMap/SelfCellController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfCellController : MonoBehaviour
+{
+    private Cell m_currentCell;     //Œ»İ©•ª‚ªŠ‘®‚µ‚Ä‚¢‚éƒZƒ‹
+
+    
+}

e89a9943cab658e3565cc2ccccfd6f80f50e655d
Maru40 <umeda.4072@gmail.com>
2023-01-17 10:02:48 +0900

    ImpactCellの生成

---
+++ VRGame03/Assets/Maruyama/Scripts/CellMap/ImpactCell.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpactCell : Cell
+{
+    /// <summary>
+    /// 影響データ
+    /// </summary>
+    public struct ImpactData
+    {
+        public float dangerValue;       //危険値
+    }
+
+    private ImpactData m_impactData;    //影響データ
+
+    public ImpactCell(int index, Parametor parametor) :
+        base(index, parametor)
+    { }
+
+    public ImpactCell(int index, Parametor parametor, ImpactData impactData):
+        base(index, parametor)
+    {
+        m_impactData = impactData;
+    }
+
+    //--------------------------------------------------------------------------------------
+    ///	アクセッサ
+    //--------------------------------------------------------------------------------------
+
+    public void SetImpactData(ImpactData data) { m_impactData = data; }
+
+    public ImpactData GetImpactData() { return m_impactData; }
+}

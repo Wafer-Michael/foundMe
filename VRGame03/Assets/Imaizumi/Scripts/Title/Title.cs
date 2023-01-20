de5f6228e2775919f80bb995bfba82a4e981d91b
Shion Imaizumi <zeal20300052@c-league.jp>
2023-01-20 14:50:06 +0900

    一定間隔で電気が消えてからシーン移動するように

--- VRGame03/Assets/Imaizumi/Scripts/Title/Title.cs
+++ VRGame03/Assets/Imaizumi/Scripts/Title/Title.cs
@@ -11,12 +11,41 @@ public class Title : MonoBehaviour
     [SerializeField]
     KeyCode m_keyCode;
 
+    [SerializeField]
+    GameObject m_lights;
+
+    [SerializeField]
+    float m_lightOffTime;
+
+    private void Start()
+    {
+        StartCoroutine("LightOff");
+    }
+
     void Update()
     {
-        if (Input.GetKeyDown(m_keyCode))
+        if(m_lights.transform.childCount <= 0)
         {
             ChangeScene();
         }
+    }
+
+    IEnumerator LightOff()
+    {
+        yield return new WaitWhile(() => !Input.GetKeyDown(m_keyCode));
+
+        while(m_lights.transform.childCount >= 0)
+        {
+            yield return new WaitForSeconds(m_lightOffTime);
+
+            var child = m_lights.transform.GetChild(0).gameObject;
+            if(child)
+            {
+                Destroy(child);
+            }
+        }
+
+        yield break;
     }
 
     void ChangeScene()

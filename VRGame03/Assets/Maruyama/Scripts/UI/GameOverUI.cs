751740fd34e0b9516152c9f8adbd3ae1fc4528ed
Maru40 <umeda.4072@gmail.com>
2023-01-23 17:02:18 +0900

    GameOverUIの実装

--- VRGame03/Assets/Maruyama/Scripts/UI/GameOverUI.cs
+++ VRGame03/Assets/Maruyama/Scripts/UI/GameOverUI.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class GameOverUI : MonoBehaviour
 {
-    TMPro.TextMeshProUGUI m_debugText;
+    private TMPro.TextMeshProUGUI m_debugText;
+
+    [SerializeField]
+    private SpriteRenderer m_spriteRender;
 
     private void Awake()
     {
         m_debugText = GetComponent<TMPro.TextMeshProUGUI>();
+
+        if(m_spriteRender == null) {
+            m_spriteRender = GetComponent<SpriteRenderer>();
+        }
     }
 
     private void Update()
     {
         if(GameManagerComponent.Instance.CurrentState == GameManagerComponent.GameState.GameOver)
         {
-            m_debugText.enabled = true;
+            //m_debugText.enabled = true;
+            m_spriteRender.enabled = true;
         }
     }
 }

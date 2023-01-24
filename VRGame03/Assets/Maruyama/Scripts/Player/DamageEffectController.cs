333e8567725beaea9a570f2f47dd08424d073d70
Maru40 <umeda.4072@gmail.com>
2023-01-24 13:18:45 +0900

    DamageEffectControllerの実装

--- VRGame03/Assets/Maruyama/Scripts/Player/DamageEffectController.cs
+++ VRGame03/Assets/Maruyama/Scripts/Player/DamageEffectController.cs
@@ -10,18 +10,40 @@ public class DamageEffectController : MonoBehaviour
     private Material m_material;
     private int m_heightPropertyID;
 
+    [SerializeField]
+    private float m_maxValue = 0.25f;
+
+    [SerializeField]
+    private float m_drawTime = 1.0f; //•\Ž¦ŽžŠÔ
+
+    private GameTimer m_timer = new GameTimer(0);
+
     private void Awake()
     {
-        
+        if (m_spriteRenderer == null)
+        {
+            m_spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        m_heightPropertyID = Shader.PropertyToID("_Height");
+        m_material = m_spriteRenderer.material;
     }
 
     private void Update()
     {
-        //m_spriteRenderer
+        if (m_timer.IsTimeUp) {
+            return;
+        }
+
+        float value = m_maxValue * m_timer.IntervalTimeRate;
+        m_material.SetFloat(m_heightPropertyID, value);
+
+        m_timer.UpdateTimer();
     }
 
-    public void FadeStart()
+    public void EffectStart()
     {
-        
+        m_material.SetFloat(m_heightPropertyID, m_maxValue);
+        m_timer.ResetTimer(m_drawTime);
     }
 }

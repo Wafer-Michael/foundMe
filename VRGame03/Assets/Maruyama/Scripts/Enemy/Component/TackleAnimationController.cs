3ce212d6761c70262c953b949e80c75d893cc133
Maru40 <umeda.4072@gmail.com>
2023-01-27 18:11:45 +0900

    TackleAnimationControllerのアップデート

--- VRGame03/Assets/Maruyama/Scripts/Enemy/Component/TackleAnimationController.cs
+++ VRGame03/Assets/Maruyama/Scripts/Enemy/Component/TackleAnimationController.cs
@@ -6,7 +6,47 @@ public class TackleAnimationController : MonoBehaviour
 {
     private enum TaskEnum
     {
-        Attack,
+        Preliminary,    //予備動作
+        Attack,         //攻撃
+        Wait,           //待機
+    }
 
+    private TaskList<TaskEnum> m_taskList = new TaskList<TaskEnum>();
+
+    private void Awake()
+    {
+        DefineTask();
+    }
+
+    private void Update()
+    {
+        //タスクが終了していたら
+        if (m_taskList.IsEnd) {
+            return;
+        }
+
+        m_taskList.UpdateTask();    //タスクのアップデート
+    }
+
+    private void DefineTask()
+    {
+        //予備動作
+
+        //攻撃タスク
+
+        //待機
+    }
+
+    private void SelectTask()
+    {
+        TaskEnum[] tasks = {
+            TaskEnum.Preliminary,
+            TaskEnum.Attack,
+            TaskEnum.Wait
+        };
+
+        foreach(var task in tasks) {
+            m_taskList.AddTask(task);
+        }
     }
 }
